b111fd92f8ee10c7cf28948a70dbde20f22aca73
tudou <13261205116@163.com>
2020-11-07 14:10:53 +0800

    :zap: clip much image

--- blog/pages/index.vue
+++ blog/pages/index.vue
@@ -1,19 +1,12 @@
 <template>
   <div class="bg">
     <div class="content-wrapper">
-      <!-- <div class="header">
+      <div class="header">
         <img src="/blog_logo.jpg" />
         <div class="header-info">
           <p class="header-title">土豆的博客</p>
-          <p>brain</p>
+          <p>Nick Pato</p>
         </div>
-      </div> -->
-      <div class="swiper-wrapper">
-        <swiper>
-          <div class="swiper-item" v-for="item in swiperItems" :key="item.value">
-            <img :src="item.img">
-          </div>
-        </swiper>
       </div>
       <div class="content">
         <nuxt-child/>
@@ -24,58 +17,8 @@
 </template>
 
 <script>
-import swiper from '@/components/swiper'
 export default {
-  name: 'root',
-  components: {
-    swiper
-  },
-  data () {
-    return {
-      swiperItems: [
-        {
-          value: 1,
-          img: '/upload/image/all.png'
-        },
-        {
-          value: 2,
-          img: '/upload/image/blk.png'
-        },
-        {
-          value: 3,
-          img: '/upload/image/flq.png'
-        },
-        {
-          value: 4,
-          img: '/upload/image/lbnm.png'
-        },
-        {
-          value: 5,
-          img: '/upload/image/lf.png'
-        },
-        {
-          value: 6,
-          img: '/upload/image/lsa.png'
-        },
-        {
-          value: 7,
-          img: '/upload/image/qb.png'
-        },
-        {
-          value: 8,
-          img: '/upload/image/sl.png'
-        },
-        {
-          value: 9,
-          img: '/upload/image/sz.png'
-        },
-        {
-          value: 10,
-          img: '/upload/image/wsp.png'
-        }
-      ]
-    }
-  }
+  name: 'home'
 }
 </script>
 
